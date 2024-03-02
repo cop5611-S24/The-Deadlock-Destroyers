8490eaeefda782057ef801541680c20e5bcf1874
Brian-Pob <brianpoblete77777@gmail.com>
2024-03-02 16:40:47 -0500

    revise setup and script

--- script.py
+++ script.py
@@ -5,17 +5,14 @@ import os
 import time
 
 settings = {
-  # brightness at 0%, 20%...100%
+  # application workloads are mutually exclusive
+  "application_workload": ["none", "game", "video720", "video1080", "browsing", "music"],
   "brightness": [0, 20, 40, 60, 80, 100],
-  "wifi": [0, 1],
   "bluetooth": [0, 1],
   "gps": [0, 1],
   "power_saving": [0, 1],
   "refresh_rate": [60, 120],
-  "game_enabled": [0, 1],
-  "video_playing_resolution": ["720p", "1080p"],
-  "browsing": [0, 1],
-  "music_playing": [0, 1]
+  # wifi always on because we need to debug via adb wirelessly
 }
 
 #df = pd.DataFrame(columns=["timestamp", "currentCharge"] + list(settings.keys()))
@@ -24,29 +21,30 @@ settings = {
 #  df = df._append(pd.Series([None, None] + list(combination), index=df.columns), ignore_index=True)
 df = pd.read_csv("output.csv")
 
-def configure_device(brightness, wifi, bluetooth, gps, power_saving, refresh_rate, game_enabled, video_playing_resolution, browsing, music_playing):
+def run_workload(workload):
+  if (workload == "browsing"):
+    subprocess.run(["adb", "shell", "am", "start", "-a", "android.intent.action.VIEW", "-d", "http://www.twitter.com"], capture_output=True, text=True)
+  elif (workload == "music"):
+    subprocess.run(["adb", "shell", "am", "start", "-a", "android.intent.action.VIEW", "spotify:playlist:4bfj9Go9YnSq7L4YeWTWeY:play"], capture_output=True, text=True)
+  elif (workload == "game"):
+    subprocess.run(["adb", "shell", "am", "start",  "-n", "com.rovio.baba/com.unity3d.player.UnityPlayerActivity"], capture_output=True, text=True)
+  elif (workload == "video720"):
+    subprocess.run(["adb", "shell", "am", "start", "-a", "android.intent.action.VIEW", "-d", "http://www.youtube.com/watch?v=YRhFSWz_J3I", "--ei", "resolution", "720"], capture_output=True, text=True)
+  elif (workload == "video1080"):
+    subprocess.run(["adb", "shell", "am", "start", "-a", "android.intent.action.VIEW", "-d", "http://www.youtube.com/watch?v=YRhFSWz_J3I", "--ei", "resolution", "1080"], capture_output=True, text=True)
+
+def configure_device(workload, brightness, bluetooth, gps, power_saving, refresh_rate):
   # brightness
   device_max_sreen_brightness_range = [22, 4095]
   device_brightness = (brightness / 100) * (device_max_sreen_brightness_range[1] - device_max_sreen_brightness_range[0]) + device_max_sreen_brightness_range[0]
   device_brightness = round(device_brightness)
   print(f"adb shell settings put system screen_brightness {device_brightness}")
-  subprocess.run(["adb", "shell", "settings", "put", "system", "screen_brightness", str(device_brightness)], capture_output=True, text=True)
-
-  # wifi
-  if wifi == 0:
-    print("adb shell svc wifi disable")
-    subprocess.run(["adb", "shell", "svc", "wifi", "disable"], capture_output=True, text=True)
-  else:
-    print("adb shell svc wifi enable")
-    subprocess.run(["adb", "shell", "svc", "wifi", "enable"], capture_output=True, text=True)
+  print( subprocess.run(["adb", "shell", "settings", "put", "system", "screen_brightness", str(device_brightness)], capture_output=True, text=True).stdout)
 
   # bluetooth
-  if bluetooth == 0:
-    print("adb shell svc bluetooth disable")
-    subprocess.run(["adb", "shell", "svc", "bluetooth", "disable"], capture_output=True, text=True)
-  else:
-    print("adb shell svc bluetooth enable")
-    subprocess.run(["adb", "shell", "svc", "bluetooth", "enable"], capture_output=True, text=True)
+  # adb shell settings put global bluetooth_disabled_profiles 1 / 0
+  print(f"adb shell settings put global bluetooth_disabled_profiles {bluetooth}")
+  print( subprocess.run( ["adb", "shell", "settings", "put", "global", "bluetooth_disabled_profiles", f"{bluetooth}"], capture_output=True, text=True).stdout)
 
   # gps
   if gps == 0:
@@ -71,41 +69,12 @@ def configure_device(brightness, wifi, bluetooth, gps, power_saving, refresh_rat
   print(f"adb shell settings put system min_refresh_rate {refresh_rate}")
   subprocess.run(["adb", "shell", "settings", "put", "system", "min_refresh_rate", str(refresh_rate)], capture_output=True, text=True)
 
-  if (browsing == 0):
-    print("adb shell am force-stop com.android.chrome")
-    subprocess.run(["adb", "shell", "am", "force-stop", "com.android.chrome"], capture_output=True, text=True)
-  else:
-    print("adb shell am start -a android.intent.action.VIEW -d http://www.twitter.com")
-    subprocess.run(["adb", "shell", "am", "force-stop", "com.android.chrome"], capture_output=True, text=True)
-    subprocess.run(["adb", "shell", "am", "start", "-a", "android.intent.action.VIEW", "-d", "http://www.twitter.com"], capture_output=True, text=True)
-  
-  if (music_playing == 0):
-    print("adb shell am force-stop com.spotify.music")
-    subprocess.run(["adb", "shell", "am", "force-stop", "com.spotify.music"], capture_output=True, text=True)
-  else:
-    print("adb shell am start -a android.intent.action.VIEW spotify:playlist:4bfj9Go9YnSq7L4YeWTWeY:play")
-    subprocess.run(["adb", "shell", "am", "force-stop", "com.spotify.music"], capture_output=True, text=True)
-    subprocess.run(["adb", "shell" , "am", "start", "-a", "android.intent.action.VIEW", "spotify:playlist:4bfj9Go9YnSq7L4YeWTWeY:play"])
-  
-  if (game_enabled == 0):
-    subprocess.run(["adb", "shell", "am", "force-stop", "com.rovio.baba"])
-  else:
-    subprocess.run(["adb", "shell", "am", "force-stop", "com.rovio.baba"])
-    subprocess.run(["adb", "shell", "am", "start",  "-n", "com.rovio.baba/com.unity3d.player.UnityPlayerActivity"])  
-  
-  
-  subprocess.run(["adb", "shell", "am", "force-stop", "com.google.android.youtube"])
-  if (video_playing_resolution == "720p"):
-    subprocess.run(["adb", "shell", "am", "start", "-a", "android.intent.action.VIEW", "-d", "http://www.youtube.com/watch?v=YRhFSWz_J3I", "--ei", "resolution", "720"])
-  elif(video_playing_resolution == "1080p"):
-    subprocess.run(["adb", "shell", "am", "start", "-a", "android.intent.action.VIEW", "-d", "http://www.youtube.com/watch?v=YRhFSWz_J3I", "--ei", "resolution", "1080"])
-
-  
-
+  run_workload(workload)
 
 
 device_max_sreen_brightness_range = [22, 4095]
 device_brightness_percentages = []
+
 for brightness_level in settings["brightness"]:
   device_brightness_percentages.append((brightness_level / 100) * (device_max_sreen_brightness_range[1] - device_max_sreen_brightness_range[0]) + device_max_sreen_brightness_range[0])
 
@@ -117,20 +86,22 @@ print(device_brightness_percentages)
 # In case the script needs to be stopped, use this variable to know where to start from
 iteration_counter = 1
 tempres = ""
-
-# Loop through each row in the dataframe
-# Use the values from the 3rd column onwards to configure the device
-# Measure the battery life
-# Store the results in a file
-# Sleep for 2 minutes.
-# Repeat
 df_copy = df.copy()
+
+"""
+  Loop through each row in the dataframe
+  Use the values from the 3rd column onwards to configure the device
+  Measure the battery life
+  Store the results in a file
+  Sleep for 2 minutes.
+  Repeat
+"""
 for index, row in df.iterrows():
 
   # ---------- CONFIGURATION COMMANDS GO HERE ----------
   configure_device(*row[2:])
   # print configurations to terminal for debugging
-  print(f"brightness: {row[2]}, wifi: {row[3]}, bluetooth: {row[4]}, gps: {row[5]}, power_saving: {row[6]}, refresh_rate: {row[7]}, game_enabled: {row[8]}, video_playing_resolution: {row[9]}, browsing: {row[10]}, music_playing: {row[11]}")
+  print(f"Configurations: {row[2:]}")
   # ----------------------------------------------------
 
   # ---------- MEASUREMENT COMMANDS GO HERE ------------
@@ -154,14 +125,17 @@ for index, row in df.iterrows():
       with open("result.csv", "a") as f:
           f.write(','.join(df.columns) + '\n')
 
-  row_string = str(row['current_time']) + "," + str(row["current_battery_level"]) + "," + str(row["brightness"]) + "," + str(row["wifi"]) + "," + str(row["bluetooth"]) + "," + str(row["gps"]) + "," + str(row["power_saving"]) + "," + str(row["refresh_rate"]) + "," + str(row["game_enabled"]) + "," + str(row["video_playing_resolution"]) + "," + str(row["browsing"]) + "," + str(row["music_playing"]) + "\n"
+  # row_string = str(row['current_time']) + "," + str(row["current_battery_level"]) + "," + str(row["brightness"]) + "," + str(row["wifi"]) + "," + str(row["bluetooth"]) + "," + str(row["gps"]) + "," + str(row["power_saving"]) + "," + str(row["refresh_rate"]) + "," + str(row["game_enabled"]) + "," + str(row["video_playing_resolution"]) + "," + str(row["browsing"]) + "," + str(row["music_playing"]) + "\n"
+  row_string = ','.join([str(x) for x in row]) + '\n'
 
   with open("result.csv", "a") as f:
       f.write(row_string)
   # ----------------------------------------------------
+
+  print(f"Iteration: {iteration_counter}")
   iteration_counter += 1
-  time.sleep(120)
-  exit(0)
+  time.sleep(10)
+  # exit(0)
 
 
 # with open("output.csv", "w") as f:
--- setup.py
+++ setup.py
@@ -4,16 +4,14 @@ import pandas as pd
 import itertools
 
 settings = {
+  # application workloads are mutually exclusive
+  "application_workload": ["none", "game", "video720", "video1080", "browsing", "music"],
   "brightness": [0, 20, 40, 60, 80, 100],
-  "wifi": [0, 1],
   "bluetooth": [0, 1],
   "gps": [0, 1],
   "power_saving": [0, 1],
   "refresh_rate": [60, 120],
-  "game_enabled": [0, 1],
-  "video_playing_resolution": ["0p", "720p", "1080p"],
-  "browsing": [0, 1],
-  "music_playing": [0, 1]
+  # wifi always on because we need to debug via adb wirelessly
 }
 
 # First step is to populate the dataframe with all the possible combinations
